3a214290821c51920aab928772e67ec48194a2f6
Subhang Mokkarala <90596655+Subhangmokkarala@users.noreply.github.com>
2023-07-19 12:38:17 +0530

    nada yada

--- timep/char2.java
+++ timep/char2.java
@@ -2,7 +2,7 @@ public class char2 {
         public static void main(String[] args) {
               
           char letter = '\u0053';
-          System.out.println(letter);  // prints Q
+          System.out.println(letter);  // printsq
         
       }
       
