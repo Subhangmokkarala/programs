b5d2952c7cdce9c52c6ba3497f5e8a14cfd8ec27
Subhang Mokkarala <90596655+Subhangmokkarala@users.noreply.github.com>
2023-08-09 12:35:36 +0530

    fix matrix.java

--- Lab/matrix.java
+++ Lab/matrix.java
@@ -1,20 +1,30 @@
-public class matrix {
+import java.util.Scanner; // Import the Scanner class
 
-  public static void matrix(string[]arg)
-  {
-    scanner scanner = new scanner(system in);
-    system outprintln ("enter the num"'a');
-    double a = scanner.next double();
-    system outprintln("enter the num"'b');
-    double b = scanner.next double();
-    system outprintln("enter the num"'c')
-    double c = scanner.next double();
-    double d = scanner.next double();
-    double determinant = cal determinant (a,b,c,d);
-    systemout.println ("the det is:"determinant);
-    scanner.(lose());
-  }
-  private static double(double a, double b, double c, double d);
-  return(a*d)-(b*c);
-}
+public class Matrix {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Use 'System.in' instead of 'system in'
+        
+        System.out.println("Enter the value for 'a':");
+        double a = scanner.nextDouble();
+        
+        System.out.println("Enter the value for 'b':");
+        double b = scanner.nextDouble();
+        
+        System.out.println("Enter the value for 'c':"); // Fix the syntax of this line
+        double c = scanner.nextDouble();
+        
+        System.out.println("Enter the value for 'd':"); // Add missing 'System.out.'
+        double d = scanner.nextDouble();
+        
+        double determinant = calculateDeterminant(a, b, c, d); // Call the calculateDeterminant method
+        
+        System.out.println("The determinant is: " + determinant); // Fix the syntax of this line
+        
+        scanner.close(); // Close the Scanner
+    }
+    
+    private static double calculateDeterminant(double a, double b, double c, double d) {
+        return (a * d) - (b * c);
+    }
 }
